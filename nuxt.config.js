eacf729f65cce9555d9d030295399dcf324442e8
Sandro Marques <sandro123iv@gmail.com>
2020-12-29 15:33:04 +0000

    New favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,28 @@ export default {
 
 	// Global page headers (https://go.nuxtjs.dev/config-head)
 	head: {
-		title: 'tsuu-web',
+		title: 'Tsuu',
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: 'Tsuu, o hub das fansubs portuguesas' }
+			{ hid: 'description', name: 'description', content: 'Tsuu, o hub das fansubs portuguesas' },
+
+			// Favicons - https://realfavicongenerator.net
+			{ hid: 'app-name-apple',  name: 'apple-mobile-web-app-title', content: 'Tsuu' },
+			{ hid: 'app-name',        name: 'application-name',           content: 'Tsuu' },
+			{ hid: 'app-ms-color',    name: 'msapplication-TileColor',    content: '#00adb5' },
+			{ hid: 'app-theme-color', name: 'theme-color',                content: '#ff0000' }
 		],
 		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+
+			// Favicons - https://realfavicongenerator.net
+			{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+			{ rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+			{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+			{ rel: 'manifest', href: '/site.webmanifest' },
+			{ rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#00adb5' }
+
 		]
 	},
 
